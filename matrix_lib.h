9989979b53a8718becfa8a58142a1a8b266ca1b0
Dhilan Ramaprasad <dhilanr@icloud.com>
2024-09-15 21:22:56 -0400

    float, too

--- matrix_lib.h
+++ matrix_lib.h
@@ -2,7 +2,8 @@
 #define MATRIX_LIB_H
 
 extern "C" {
-    void print_matrix(const int* matrix, int num_rows, int num_cols);
+    void print_int_matrix(const int* matrix, int num_rows, int num_cols);
+    void print_float_matrix(const float* matrix, int num_rows, int num_cols);
 }
 
 #endif // MATRIX_LIB_H
